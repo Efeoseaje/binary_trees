a589daa33eca002ea53320bfa22a73a77b572d26
Efeoseaje <efeoseaje@yahoo.com>
2023-06-02 01:12:05 +0100

    Delete Binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes the entire binary tree
+ * 
+ * @tree: Pointer to the root node of the tree to delete
+ * 
+ * Return: 0
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	free(tree);
+}
